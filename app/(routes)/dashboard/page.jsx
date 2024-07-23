4a77429dc928645e27a771ede65b073a0affcc39
cathymiao04 <cathy@puygroup.com>
2024-07-23 18:35:41 -0500

    Set up Nav Bar

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,10 +1,10 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import { UserButton } from '@clerk/nextjs'
 
 function Dashboard() {
   return (
     <div>
-      <UserButton />
+      Dashboard
     </div>
   )
 }
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -2,13 +2,18 @@
 
 import React from 'react'
 import Image from "next/image";
-import { Button } from '@/components/ui/button';
 import { useUser, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 function Header() {
 
   const { user, isSignedIn } = useUser();
+
+  const Button = ({ children }) => (
+    <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      {children}
+    </button>
+  );
 
   return (
     <div className='p-5 flex justify-between items-center border shadow-sm'>
