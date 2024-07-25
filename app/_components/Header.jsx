09ccab91a401e6ad2997ad37d2d530e811918315
cathymiao04 <cathy@puygroup.com>
2024-07-24 20:18:02 -0500

    Implemented create new budget functionality

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,17 +3,12 @@
 import React from 'react'
 import Image from "next/image";
 import { useUser, UserButton } from "@clerk/nextjs";
+import { Button } from "../../components/ui/button"
 import Link from "next/link";
 
 function Header() {
 
   const { user, isSignedIn } = useUser();
-
-  const Button = ({ children }) => (
-    <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-      {children}
-    </button>
-  );
 
   return (
     <div className='p-5 flex justify-between items-center border shadow-sm'>
