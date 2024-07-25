3acb3c99cedeab4957c9c6f88745c30d4471b9f2
cathymiao04 <cathy@puygroup.com>
2024-07-24 21:09:46 -0500

    Set up display of budget list

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -2,5 +2,6 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+// const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+const sql = neon('postgresql://expense-tracker_owner:tC80qBEpWGUH@ep-dry-tree-a5dfo00n.us-east-2.aws.neon.tech/expense-tracker?sslmode=require');
 export const db = drizzle(sql, { schema });
